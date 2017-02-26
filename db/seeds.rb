d2af6f0036f0017d242a0998a83bfecacd37e2b4
Joel Williams & Anna Pougas <pair+Joel-b-williams.karyatis@devbootcamp.com>
2017-02-25 19:48:20 -0600

    imperfect random route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Waypoint.create(location: 'POINT(41.876264 -87.653237)') #0dbc
-Waypoint.create(location: 'POINT(41.879635 -87.647251)') #1dugans
-Waypoint.create(location: 'POINT(41.876711 -87.657272)') #2wise owl
-Waypoint.create(location: 'POINT(41.874868 -87.658079)') #3UIC
-Waypoint.create(location: 'POINT(41.875501 -87.648917)') #4blue line
-Waypoint.create(location: 'POINT(41.871930 -87.648022)') #5student center
-Waypoint.create(location: 'POINT(41.884634 -87.647656)') #6Au Cheval
+Waypoint.create(location: 'POINT(41.876264 -87.653237)') #0 4dbc
+Waypoint.create(location: 'POINT(41.879635 -87.647251)') #1 5dugans
+Waypoint.create(location: 'POINT(41.876711 -87.657272)') #2 6wise owl
+Waypoint.create(location: 'POINT(41.874868 -87.658079)') #3 7UIC
+Waypoint.create(location: 'POINT(41.875501 -87.648917)') #4 8blue line
+Waypoint.create(location: 'POINT(41.871930 -87.648022)') #5 9student center
+Waypoint.create(location: 'POINT(41.884634 -87.647656)') #6 10Au Cheval
