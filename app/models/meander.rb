413c9774521e8da9975366125663b540577919b2
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2017-02-27 21:43:03 -0600

    continue refactor of algorithm to incorporate postgis

--- app/models/meander.rb
+++ app/models/meander.rb
@@ -4,43 +4,61 @@ class Meander < ApplicationRecord
 
   def meander(current_waypoint, destination)
     # 150 m min
-    min_search_radius = min_distance
+    min_search_radius = 150
     # 800 m max
-    max_search_radius = max_distance
-    route_path(current_waypoint, destination, 0.1).slice(0..-2)
+    max_search_radius = 800
+    distance_to_end = self.initial_distance(current_waypoint, destination)
+    route_path(current_waypoint, destination, distance_to_end, min_search_radius, max_search_radius).slice(0..-2)
   end
 
-  def route_path(current_waypoint, destination, modifier, points_of_interest = [])
-    next_waypoint = choose_next_waypoint(current_waypoint, destination, modifier)
+  def route_path(current_waypoint, destination, distance_to_end, min_search_radius, max_search_radius, points_of_interest = [])
+    next_waypoint = choose_next_waypoint(current_waypoint, destination, min_search_radius, max_search_radius)
     if points_of_interest.length > 20
       return points_of_interest
     end
     if next_waypoint == nil
-      if modifier < 1.0
-        modifier += 0.1
-        next_waypoint = route_path(current_waypoint, destination, modifier, points_of_interest)
+      if max_search_radius < distance_to_end
+        min_search_radius += 650
+        max_search_radius += 650
+        next_waypoint = route_path(current_waypoint, destination, min_search_radius, max_search_radius, points_of_interest)
       else
         return points_of_interest
         #stop adding points and route to destination
       end
     else
       points_of_interest << next_waypoint
-      route_path(next_waypoint, destination, 0.1, points_of_interest)
+      min_search_radius = 150
+      max_search_radius = 800
+      route_path(next_waypoint, destination, min_search_radius, max_search_radius, points_of_interest)
     end
     # add to aggregate array of coords/waypoints => return it
   end
 
-  def choose_next_waypoint(current_waypoint, destination, modifier)
-    # find all waypoints within radius of current waypoint
-
-    waypoints_in_radius = Waypoint.waypoints_in_radius_of(current_waypoint, min_search_radius, max_search_radius)
-    # select those that are closer to end point
-    waypoints_nearer_destination = waypoints_in_radius.select { |waypoint| waypoint.closer_to_end(current_waypoint, destination)}
-    # select one from ^ at random
-    next_waypoint = waypoints_nearer_destination.sample
+  def choose_next_waypoint(current_waypoint, destination, min_search_radius, max_search_radius)
+    current_waypoint.find_potential_next_waypoints(destination, min_search_radius, max_search_radius).sample
   end
 
+  def initial_distance(start_point, end_point)
+    long1 = start_point.location.x
+    p long1
+    long2 = end_point.location.x
+    lat1 = start_point.location.y
+    lat2 = end_point.location.y
+    dtor = Math::PI/180
+    r = 6378.14*1000
 
+    rlat1 = lat1 * dtor 
+    rlong1 = long1 * dtor 
+    rlat2 = lat2 * dtor 
+    rlong2 = long2 * dtor 
+
+    dlon = rlong1 - rlong2
+    dlat = rlat1 - rlat2
+
+    a = (Math::sin(dlat/2)**2) + Math::cos(rlat1) * Math::cos(rlat2) * (Math::sin(dlon/2)**2)
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+    d = r * c
+  end
 
   def self.assemble_meander_coordinates(array_waypoints)
     array_waypoints.map { |waypoint| waypoint.location.coordinates }
--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -4,21 +4,15 @@ class Waypoint < ApplicationRecord
   # Returns points in our circular search band
   def self.waypoints_in_radius_of(current_waypoint, min_search_radius, max_search_radius) 
     #point, distance
+    rando = 'SPHEROID["WGS 84", 6378137, 298.257223563]'
     waypoint_text = current_waypoint.location.as_text
     sql_query = 
-      "ST_DistanceSphere(location, ST_GeomFromText('#{waypoint_text}')) < #{max_search_radius} AND ST_DistanceSphere(location, ST_GeomFromText('#{waypoint_text}')) > #{min_search_radius}"
+      "ST_Distance_Spheroid(location, ST_GeomFromText('#{waypoint_text}'), '#{rando}') < #{max_search_radius} AND ST_Distance_Spheroid(location, ST_GeomFromText('#{waypoint_text}'), '#{rando}') > #{min_search_radius}"
     Waypoint.where(sql_query)
   end
 
   # Returns points closer to the endpoint
   def self.find_waypoints_closer_to_end(current_waypoint, destination)
-    # #thing.location.x // thing.location.y
-    # x1 = location.x
-    # x2 = destination.location.x
-    # y1 = location.y
-    # y2 = destination.location.y
-    # distance_start_to_end = Math.hypot(x2-x1,y2-y1)
-
     current_text = current_waypoint.location.as_text
     destination_text = destination.location.as_text
     sql_query = "ST_DistanceSphere(location, ST_GeomFromText('#{destination_text}')) < ST_DistanceSphere(ST_GeomFromText('#{current_text}'), ST_GeomFromText('#{destination_text}'))"
@@ -28,26 +22,39 @@ class Waypoint < ApplicationRecord
   def find_potential_next_waypoints(destination, min_search_radius, max_search_radius)
     waypoints_in_band = Waypoint.waypoints_in_radius_of(self, min_search_radius, max_search_radius)
     waypoints_closer = Waypoint.find_waypoints_closer_to_end(self, destination)
-    p waypoints_in_band
-    p waypoints_closer
-    
+    waypoint_options = (waypoints_in_band & waypoints_closer)
   end
 
+  # set default modifier to 0.1 in variable?
+  # def search_radius(full_distance, modifier)
+  #   #set standard modifier to be 0.1
+  #   radius = full_distance * modifier
+  # end
 
-  def closer_to_end(current_waypoint, destination)
-    return self.find_distance_to(destination) < current_waypoint.find_distance_to(destination)
-  end
-
-
-# set default modifier to 0.1 in variable?
-  def search_radius(full_distance, modifier)
-    #set standard modifier to be 0.1
-    radius = full_distance * modifier
-  end
-  
-  def distance_conversion(lat1, lat2)
-    latMid = (Lat1+Lat2 )/2.0;
-    m_per_deg_lat = 111132.954 - 559.822 * cos( 2 * latMid ) + 1.175 * cos( 4 * latMid);
-    m_per_deg_lon = 111132.954 * cos( latMid );
-  end
 end
+
+  # def initial_distance(destination)
+  #   # latitude_midpoint = (self.location.x + destination.location.x)/2.0
+  #   # # p latitude_midpoint
+  #   # m_per_deg_lat = 111132.954 - 559.822 * Math.cos( 2 * latitude_midpoint ) + 1.175 * Math.cos( 4 * latitude_midpoint)
+  #   # m_per_deg_lon = 111132.954 * Math.cos( latitude_midpoint )
+  #   current_text = location.as_text
+  #   destination_text = destination.location.as_text
+
+  #   sql_query = "ST_Distance(ST_GeomFromText('#{current_text}'), ST_GeomFromText('#{destination_text}'))"
+  #   Waypoint.select(sql_query)
+  # end
+  
+  # def distance_conversion(lat1, lat2)
+  #   latMid = (Lat1+Lat2 )/2.0;
+  #   m_per_deg_lat = 111132.954 - 559.822 * cos( 2 * latMid ) + 1.175 * cos( 4 * latMid);
+  #   m_per_deg_lon = 111132.954 * cos( latMid );
+  # end
+
+  # PATRICK"S PERSONAL MISSION>>>>
+  # def find_distance_to_end(destination)
+  #   current_text = location.as_text
+  #   destination_text = destination.location.as_text
+  #   sql_query = "ST_Distance_Sphere(location ST_GeomFromText('#{current_text}'), location ST_GeomFromText('#{destination_text}'))"
+  #   Waypoint.where(sql_query)
+  # end
