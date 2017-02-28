764504c3228dc0972d0768c30e059cf63b58d2d7
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2017-02-27 22:37:43 -0600

    Have recursive function working with postgis calls

--- app/models/meander.rb
+++ app/models/meander.rb
@@ -8,39 +8,69 @@ class Meander < ApplicationRecord
     # 800 m max
     max_search_radius = 800
     distance_to_end = self.initial_distance(current_waypoint, destination)
+    p "Dist" * 80
+    p distance_to_end
     route_path(current_waypoint, destination, distance_to_end, min_search_radius, max_search_radius).slice(0..-2)
   end
 
   def route_path(current_waypoint, destination, distance_to_end, min_search_radius, max_search_radius, points_of_interest = [])
+    p "*"*60
+    p "calls function"
+    p "here are our variables"
+    p current_waypoint
+    p destination
+    p distance_to_end
+    p min_search_radius
+    p max_search_radius
+    p points_of_interest
     next_waypoint = choose_next_waypoint(current_waypoint, destination, min_search_radius, max_search_radius)
+    p "*"*50
+    p "chooses next waypoint"
+    p next_waypoint
     if points_of_interest.length > 20
       return points_of_interest
     end
     if next_waypoint == nil
+      p "*"*50
+      p 'waypoint was nil'
       if max_search_radius < distance_to_end
         min_search_radius += 650
         max_search_radius += 650
-        next_waypoint = route_path(current_waypoint, destination, min_search_radius, max_search_radius, points_of_interest)
+        next_waypoint = route_path(current_waypoint, destination, distance_to_end, min_search_radius, max_search_radius, points_of_interest)
       else
         return points_of_interest
         #stop adding points and route to destination
       end
     else
+      p "*"*50
+      p 'found a waypoint'
       points_of_interest << next_waypoint
+      p '*'*100
+      p points_of_interest
       min_search_radius = 150
       max_search_radius = 800
-      route_path(next_waypoint, destination, min_search_radius, max_search_radius, points_of_interest)
+      route_path(next_waypoint, destination, distance_to_end, min_search_radius, max_search_radius, points_of_interest)
     end
     # add to aggregate array of coords/waypoints => return it
   end
 
+  # returning one waypoint from the array of next potential options
+  # good on standalone test
   def choose_next_waypoint(current_waypoint, destination, min_search_radius, max_search_radius)
-    current_waypoint.find_potential_next_waypoints(destination, min_search_radius, max_search_radius).sample
+    p "*"*50
+    p "inside choose next"
+    p "variables"
+    p current_waypoint
+    p destination
+    p min_search_radius
+    p max_search_radius
+    p current_waypoint.find_potential_next_waypoints(destination, min_search_radius, max_search_radius).sample
   end
 
+  # Calculates distance between start and end point at beginning of Meander / Walk
+  # Good on standalone test
   def initial_distance(start_point, end_point)
     long1 = start_point.location.x
-    p long1
     long2 = end_point.location.x
     lat1 = start_point.location.y
     lat2 = end_point.location.y
--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -4,8 +4,15 @@ class Waypoint < ApplicationRecord
   # Returns points in our circular search band
   def self.waypoints_in_radius_of(current_waypoint, min_search_radius, max_search_radius) 
     #point, distance
+    p "*"*50
+    p "inside waypoints_in_rad"
+    p "variables, current/min/max"
+    p current_waypoint
+    p min_search_radius
+    p max_search_radius
     rando = 'SPHEROID["WGS 84", 6378137, 298.257223563]'
     waypoint_text = current_waypoint.location.as_text
+    p waypoint_text
     sql_query = 
       "ST_Distance_Spheroid(location, ST_GeomFromText('#{waypoint_text}'), '#{rando}') < #{max_search_radius} AND ST_Distance_Spheroid(location, ST_GeomFromText('#{waypoint_text}'), '#{rando}') > #{min_search_radius}"
     Waypoint.where(sql_query)
@@ -20,7 +27,11 @@ class Waypoint < ApplicationRecord
   end
 
   # compares the two arrays and returns array of potential next waypoints in radius and closer to destination.
+  # Good on standalone test
   def find_potential_next_waypoints(destination, min_search_radius, max_search_radius)
+    p "*"* 50
+    p "inside find-potential"
+    p self
     waypoints_in_band = Waypoint.waypoints_in_radius_of(self, min_search_radius, max_search_radius)
     waypoints_closer = Waypoint.find_waypoints_closer_to_end(self, destination)
     waypoint_options = (waypoints_in_band & waypoints_closer)
