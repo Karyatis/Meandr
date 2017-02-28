9339164440ee400ed46216b3bca9d1715b5a7888
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2017-02-28 02:49:05 -0600

    update to schema file typo removed

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20170226005304) do
 
   create_table "waypoints", force: :cascade do |t|
     t.geometry "location",   limit: {:srid=>0, :type=>"point"}
-    t.line_string 
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
   end
