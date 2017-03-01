50509678b0327a83757e35a3b5189ba08a18f132
Hesham Elrehaimy <email": null>
2017-02-28 18:10:12 -0600

    Test models and welcome controller:

--- spec/models/meander_spec.rb
+++ spec/models/meander_spec.rb
@@ -1,71 +1,45 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe Meander do
+describe Meander do
 
-#   let(:meander) { Meander.create! }
-#   let(:dbc) { Waypoint.create!(location: 'POINT(41.876451 -87.653213)') }
-#   let(:dugans) { Waypoint.create!(location: 'POINT(41.879618 -87.647487)') }
-#   let(:wise_owl) { Waypoint.create!(location: 'POINT(41.876647 -87.657238)') }
-#   let(:uic) { Waypoint.create!(location: 'POINT(41.874853 -87.656140)') }
-#   let(:blue_line) { Waypoint.create!(location: 'POINT(41.875540 -87.647181)') }
-#   let(:student_center) { Waypoint.create!(location: 'POINT(41.871955 -87.647998)') }
-#   let(:au_cheval) { Waypoint.create!(location: 'POINT(41.884659 -87.647666)') }
+  before(:each) do
+    @meander = Meander.create!
+    @dbc = Waypoint.create!(location: 'POINT(-87.653217 41.876448)')
+    @dugans = Waypoint.create!(location: 'POINT(-87.647490 41.879617)')
+    @wise_owl = Waypoint.create!(location: 'POINT(-87.657240 41.876645)')
+    @uic = Waypoint.create!(location: 'POINT(-87.656140 41.874853)')
+    @blue_line = Waypoint.create!(location: 'POINT(-87.647180 41.875540)')
+    @student_center = Waypoint.create!(location: 'POINT(-87.648000 41.871955)')
+     @au_cheval = Waypoint.create!(location: 'POINT(-87.647670 41.884654)')
+  end
 
-#   describe "#choose_next_waypoint" do
-#     it "returns a waypoint object if there is one" do
-#       meander
-#       dbc
-#       dugans
-#       wise_owl
-#       uic
-#       blue_line
-#       student_center
-#       au_cheval
+  describe "#assemble_meander_coordinates" do
+    it "returns an array with reversed coordinates of the waypoints" do
+      expect(Meander.assemble_meander_coordinates([@dbc, @uic])).to eq [[41.876448, -87.653217], [41.874853, -87.656140]]
+    end
+  end
 
-#       expect(meander.choose_next_waypoint(dbc, au_cheval, 0.7)).to be_an_instance_of(Waypoint)
-#     end
+  describe "#initial_distance" do
+    it "returns the distance between the start point and end point" do
+      expect(@meander.initial_distance(@dbc, @au_cheval)).to eq 1022.6561812623268
+    end
+  end
 
-#     it "returns nil if there are no waypoints" do
-#       meander
-#       dbc
-#       dugans
-#       wise_owl
-#       uic
-#       blue_line
-#       student_center
-#       au_cheval
+  describe "#choose_next_waypoint" do
+    it "returns a possible next waypoint" do
+      expect(@meander.choose_next_waypoint(@dbc, @au_cheval, 150, 800)).to be_an_instance_of Waypoint
+    end
+  end
 
-#       expect(meander.choose_next_waypoint(uic, blue_line, 0.3)).to eq nil
-#     end
-#   end
+  describe "#route_path" do
+    it "returns an array of possible next waypoints" do
+      expect(@meander.route_path(@dbc, @au_cheval, 1022.6561812623268, 150, 800)).to all(be_an_instance_of Waypoint)
+    end
+  end
 
-#   describe "#route_path" do
-#     it "returns an array with points of interest" do
-#        meander
-#       dbc
-#       dugans
-#       wise_owl
-#       uic
-#       blue_line
-#       student_center
-#       au_cheval
-
-#       expect(meander.route_path(dbc, au_cheval, 0.7)[0]).to be_an_instance_of(Waypoint)
-#     end
-#   end
-
-#   describe "#meander" do
-#     it "returns an array with a possible route" do
-#       meander
-#       dbc
-#       dugans
-#       wise_owl
-#       uic
-#       blue_line
-#       student_center
-#       au_cheval
-
-#       expect(meander.meander(dbc, au_cheval)[0]).to be_an_instance_of(Waypoint)
-#     end
-#   end
-# end
+  describe "#meander" do
+    it "returns an array with a possible route" do
+      expect(@meander.meander(@dbc, @au_cheval)).to all(be_an_instance_of Waypoint)
+    end
+  end
+end
